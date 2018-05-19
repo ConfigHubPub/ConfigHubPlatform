bd0db18f7b3d7ace15d1c026008f18af5db80bc3
Edin Arslanagic <edoarsla@gmail.com>
2018-05-18 20:13:29 -0500

    Update files to reflect GNU General Public Licence

--- rest/src/main/webapp/framework/dirPagination.js
+++ rest/src/main/webapp/framework/dirPagination.js
@@ -1,17 +1,18 @@
-/**
- * dirPagination - AngularJS module for paginating (almost) anything.
+/*
+ * This file is part of ConfigHub.
  *
+ * ConfigHub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Credits
- * =======
+ * ConfigHub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Daniel Tabuenca: https://groups.google.com/d/msg/angular/an9QpzqIYiM/r8v-3W1X5vcJ
- * for the idea on how to dynamically invoke the ng-repeat directive.
- *
- * I borrowed a couple of lines and a few attribute names from the AngularUI Bootstrap project:
- * https://github.com/angular-ui/bootstrap/blob/master/src/pagination/pagination.js
- *
- * Copyright 2014 Michael Bromley <michael@michaelbromley.co.uk>
+ * You should have received a copy of the GNU General Public License
+ * along with ConfigHub.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 (function() {
