c39fe894cd67c094db51c9afea4b8e2d6360285c
edin <earslanagic@p-44.com>
2018-11-05 07:29:43 -0600

    Signup is a Local account

--- rest/src/main/java/com/confighub/api/auth/Signup.java
+++ rest/src/main/java/com/confighub/api/auth/Signup.java
@@ -35,16 +35,17 @@ import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.Map;
 
-@Path("/signup")
-@Produces("application/json")
+
+@Path( "/signup" )
+@Produces( "application/json" )
 public class Signup
 {
 
     @AuthenticationNotRequired
     @POST
-    public Response signup(@FormParam("email") String email,
-                           @FormParam("username") String username,
-                           @FormParam("password") String password)
+    public Response signup( @FormParam( "email" ) String email,
+                            @FormParam( "username" ) String username,
+                            @FormParam( "password" ) String password )
     {
         Store store = new Store();
         JsonObject json = new JsonObject();
@@ -57,52 +58,64 @@ public class Signup
             // --------------------------------------------------------------------------------------------
             Map<String, String> errors = new HashMap<>();
 
-            if (Utils.isBlank(email))
-                errors.put("error_email", "Please enter email address");
-
-            if (Utils.isBlank(username))
-                errors.put("error_username", "Username required");
-
-            if (Utils.isBlank(password))
-                errors.put("error_password", "Please enter password");
-
-            if (store.isEmailRegistered(email))
-                errors.put("message", "Email is already registered");
-
-            if (store.isUsernameTaken(username))
-                errors.put("error_username", "Username is already registered");
-
-
-            if (errors.size() != 0)
+            if ( Utils.isBlank( email ) )
             {
-                for (String k : errors.keySet())
-                    json.addProperty(k, errors.get(k));
-                json.addProperty("success", false);
+                errors.put( "error_email", "Please enter email address" );
+            }
 
-                return Response.ok(gson.toJson(json), MediaType.APPLICATION_JSON).build();
+            if ( Utils.isBlank( username ) )
+            {
+                errors.put( "error_username", "Username required" );
+            }
+
+            if ( Utils.isBlank( password ) )
+            {
+                errors.put( "error_password", "Please enter password" );
+            }
+
+            if ( store.isEmailRegistered( email ) )
+            {
+                errors.put( "message", "Email is already registered" );
+            }
+
+            if ( store.isUsernameTaken( username ) )
+            {
+                errors.put( "error_username", "Username is already registered" );
+            }
+
+
+            if ( errors.size() != 0 )
+            {
+                for ( String k : errors.keySet() )
+                {
+                    json.addProperty( k, errors.get( k ) );
+                }
+                json.addProperty( "success", false );
+
+                return Response.ok( gson.toJson( json ), MediaType.APPLICATION_JSON ).build();
             }
 
             store.begin();
-            UserAccount user = store.createUser(email, username, password);
+            UserAccount user = store.createUser( email, username, password, UserAccount.AccountType.LOCAL );
             store.commit();
 
-            String token = Auth.createUserToken(user);
-            json.addProperty("token", token);
-            json.addProperty("success", true);
+            String token = Auth.createUserToken( user );
+            json.addProperty( "token", token );
+            json.addProperty( "success", true );
 
-            return Response.ok(gson.toJson(json), MediaType.APPLICATION_JSON).build();
+            return Response.ok( gson.toJson( json ), MediaType.APPLICATION_JSON ).build();
         }
-        catch (ConfigException e)
+        catch ( ConfigException e )
         {
-            System.out.println("---------------------------");
+            System.out.println( "---------------------------" );
             e.printStackTrace();
-            System.out.println("---------------------------");
+            System.out.println( "---------------------------" );
 
             store.rollback();
-            json.addProperty("success", false);
-            json.addProperty("message", e.getErrorCode().getMessage());
+            json.addProperty( "success", false );
+            json.addProperty( "message", e.getErrorCode().getMessage() );
 
-            return Response.ok(gson.toJson(json), MediaType.APPLICATION_JSON).build();
+            return Response.ok( gson.toJson( json ), MediaType.APPLICATION_JSON ).build();
         }
         finally
         {
