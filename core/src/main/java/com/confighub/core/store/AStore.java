efb679a86977493a0ca31770c3fc3fba07b1d9d9
edin <earslanagic@p-44.com>
2018-11-07 10:13:59 -0600

    disable verbose logging

--- core/src/main/java/com/confighub/core/store/AStore.java
+++ core/src/main/java/com/confighub/core/store/AStore.java
@@ -40,7 +40,7 @@ public abstract class AStore
 {
     private static final Logger log = LogManager.getLogger( AStore.class );
 
-    public static boolean verbose = true;
+    public static boolean verbose = false;
 
     /**
      * <url>http://stackoverflow.com/questions/4543947/when-should-entitymanagerfactory-instance-be-created-opened</url>
