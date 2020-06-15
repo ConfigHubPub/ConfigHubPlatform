0c6141c374eb5434594224424326192794a99fe6
William Grim <grimwm@gmail.com>
2020-06-15 14:34:46 -0500

    Bump to 1.9.2 and fix docker.
    Tweaked the development docker setup so that it
    works in more use-cases.  In reality, it could
    probably even run as a standalone docker
    production app now.

--- docker/layer/confighubdev.sh
+++ docker/layer/confighubdev.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ "${VERBOSE_STARTUP}" == "yes" ] ; then
+  set -x
+fi
+
 export ALLOCATED_MEMORY=${ALLOCATED_MEMORY:-4g}
 export HTTP_PORT=${HTTP_PORT:-80}
 export HTTPS_PORT=${HTTPS_PORT:-443}
@@ -8,33 +12,33 @@ export KEYSTORE_FILE=${KEYSTORE_FILE:-cert/confighub_default.jks}
 export KEYSTORE_ALIAS=${KEYSTORE_ALIAS:-confighub}
 export KEYSTORE_PASSWORD=${KEYSTORE_PASSWORD:-confighub}
 
-while ! mysqladmin ping -h${DB_HOST} -u${DB_SUPERUSER} -p${DB_SUPERPASS} ; do
-  echo "Waiting for mariadb to start..."
+echo "Waiting for mariadb to start..."
+while ! mysqladmin status -h"${DB_HOST}" -u"${DB_SUPERUSER}" -p"${DB_SUPERPASS}"  2>/dev/null ; do
   sleep 1
 done
 
 # Ensure confighub can access the database.  Sometimes this can be disrupted when restoring the database server
 # from a backup file when running tests.
 envsubst < /grant.sql.tpl > /tmp/grant.sql
-mysql -h${DB_HOST} -u${DB_SUPERUSER} -p${DB_SUPERPASS} < /tmp/grant.sql || exit $?
+mysql -h"${DB_HOST}" -u"${DB_SUPERUSER}" -p"${DB_SUPERPASS}" < /tmp/grant.sql || exit $?
 
-# Initialize the database if it hasn't already been done.
-echo "show tables like 'email';" > /tmp/db.init
-mysql -h${DB_HOST} -u${DB_USER} -p${DB_PASS} -D${DB_NAME} < /tmp/db.init | grep email >/dev/null
-if [[ $? -ne 0 ]] ; then
-  echo "Initializing database..."
-  java -jar /ConfigHubDBManager.jar -t mysql -r jdbc:mysql://${DB_HOST}:3306/${DB_NAME} -u${DB_USER} -p${DB_PASS} || exit $?
-else
-  echo "Database previously initialized."
-fi
+echo "Initializing database..."
+java -jar /ConfigHubDBManager.jar -t mysql -r "jdbc:mysql://${DB_HOST}:3306/${DB_NAME}" -u"${DB_USER}" -p"${DB_PASS}" || exit $?
+
+sed -i "s/127.0.0.1/${DB_HOST}/g" /confighub/server/conf/tomee.xml
+sed -i "s/<Database_Name>/${DB_NAME}/g" /confighub/server/conf/tomee.xml
+sed -i "s/<username>/${DB_USER}/g" /confighub/server/conf/tomee.xml
+sed -i "s/<password>/${DB_PASS}/g" /confighub/server/conf/tomee.xml
 
 # Always do this step so the instance always comes up with the latest development package.
-WEBAPPS_ROOT=/usr/local/tomee/webapps/ROOT
+WEBAPPS_ROOT=/confighub/server/webapps/ROOT
 rm -rf ${WEBAPPS_ROOT} || exit $?
+mv /ROOT.war ${WEBAPPS_ROOT}.war || exit $?
 echo "Unzipping ROOT.war..."
+mkdir ${WEBAPPS_ROOT}
 unzip -o ${WEBAPPS_ROOT}.war -d ${WEBAPPS_ROOT} >/dev/null || exit $?
 
 echo "Writing ${WEBAPPS_ROOT}/WEB-INF/resources.xml"
 envsubst < /dev-resources.xml.tpl > ${WEBAPPS_ROOT}/WEB-INF/resources.xml || exit $?
 
-catalina.sh run
+supervisord --nodaemon --configuration /etc/supervisord.conf
